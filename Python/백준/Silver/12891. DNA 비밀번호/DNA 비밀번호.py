a05171793c33ea93ee01e6f44b6cd60caeff379e
SECHANG1412 <binaryblitz1412@gmail.com>
2026-01-04 18:35:34 +0900

    [Silver II] Title: DNA 비밀번호, Time: 428 ms, Memory: 41096 KB -BaekjoonHub

--- Python/백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
+++ Python/백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -1,44 +1,63 @@
-import sys
-input = sys.stdin.readline
+checkArr = [0] * 4
+myArr = [0] * 4
+checkSecret = 0
 
-S, P = map(int, input().split())           
-DNA = input().strip()                      
-required = list(map(int, input().split())) 
+# 함수 정의
+def myadd(c): #새로 들어온 문자를 처리하는 함수
+    global checkArr,myArr,checkSecret
+    if c == 'A':
+        myArr[0] += 1
+        if myArr[0] == checkArr[0]:
+            checkSecret += 1
+    elif c == 'C':
+        myArr[1] += 1
+        if myArr[1] == checkArr[1]:
+            checkSecret += 1
+    elif c == 'G':
+        myArr[2] += 1
+        if myArr[2] == checkArr[2]:
+            checkSecret += 1
+    elif c == 'T':
+        myArr[3] += 1
+        if myArr[3] == checkArr[3]:
+            checkSecret += 1
 
-current = [0, 0, 0, 0]  
+def myremove(c): #제거되는 문자를 처리하는 함수
+    global checkArr, myArr, checkSecret
+    if c == 'A':
+        if myArr[0] == checkArr[0]:
+            checkSecret -= 1
+        myArr[0] -= 1
+    elif c == 'C':
+        if myArr[1] == checkArr[1]:
+            checkSecret -= 1
+        myArr[1] -= 1
+    elif c == 'G':
+        if myArr[2] == checkArr[2]:
+            checkSecret -= 1
+        myArr[2] -= 1
+    elif c == 'T':
+        if myArr[3] == checkArr[3]:
+            checkSecret -= 1
+        myArr[3] -= 1
 
-def char_to_index(ch):
-    if ch == 'A':
-        return 0
-    elif ch == 'C':
-        return 1
-    elif ch == 'G':
-        return 2
-    elif ch == 'T':
-        return 3
-
-for i in range(P):
-    idx = char_to_index(DNA[i])
-    current[idx] += 1
-
-def is_valid():
-    for i in range(4):
-        if current[i] < required[i]:
-            return False
-    return True
-
-count = 0
-if is_valid():
-    count += 1
-
+S, P = map(int, input().split())
+Result = 0
+A = list(input())
+checkArr = list(map(int, input().split()))
+for i in range(4):
+    if checkArr[i] == 0:
+        checkSecret += 1
+for i in range(P):  #초기 P 부분 문자열 처리 부분
+    myadd(A[i])
+if checkSecret == 4: #4자릿수와 관련된 크기가 모두 충족될 때 유효한 비밀번호
+    Result += 1
 for i in range(P, S):
-    left_idx = char_to_index(DNA[i - P])
-    current[left_idx] -= 1
+    j = i - P
+    myadd(A[i])
+    myremove(A[j])
+    if checkSecret == 4:
+        Result += 1
+print(Result)
 
-    right_idx = char_to_index(DNA[i])
-    current[right_idx] += 1
 
-    if is_valid():
-        count += 1
-
-print(count)
